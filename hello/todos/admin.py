1aa648bd4f21a5f9a0e575f85c54e8098374b02e
Awel <aweloe@gmail.com>
2020-10-22 23:07:23 +0300

    admin more configured

--- hello/todos/admin.py
+++ hello/todos/admin.py
@@ -5,7 +5,13 @@ from .models import Person, Todos, EmployeeInfo, LessID
 
 #customize admin panel
 class TodosAdmin(admin.ModelAdmin):
-    list_display = ( "id" ,"task" , "day", "time")
+    list_display = ( "id" ,"task" , "day", "time", "get_person")
+    def get_person(self, obj):
+        return obj.person.user.username
+    #Allows column order sorting
+    get_person.admin_order_field  = 'firstname' 
+    #Renames column head
+    get_person.short_description = 'Person Name' 
 
 admin.site.register(Person)
 #register Todos with custom setting (above)
