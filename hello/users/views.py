608994218969c0e09f7ec41f227f6b6751944086
Awel <aweloe@gmail.com>
2020-11-03 11:12:56 +0200

    builtin forms used

--- hello/users/views.py
+++ hello/users/views.py
@@ -3,12 +3,18 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django import  forms
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-group'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-group'}))
 
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("users:login"))
     return render(request, "todos/index.html")
-
+'''
 def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
@@ -24,9 +30,25 @@ def login_view(request):
                 "message" : "Invalid credentials."
             })
     return render(request, "users/login.html")
-
+'''
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user=form.get_user()
+            login(request, user)
+            return HttpResponseRedirect(reverse("todos:index"), {
+                "user": request.user.first_name
+            })
+        else:
+            return render(request, "users/login.html", {
+                "message" : "Invalid credentials.",
+                 "form" : form
+            })
 def logout_view(request):
+    form = LoginForm()
     logout(request)
     return render(request, "users/login.html", {
-        "message" : "Logged out."
+        "message" : "Logged out.",
+        "form" : form
     })
